ce301832d5b25a2cf8aa31b18a4cf6c23c9e1034
bsisnsvsj <ab.sk.19177@gmail.com>
2025-06-17 00:39:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env, ctx) {
-    return new Response("V2Ray Worker is up and running!", {
-      headers: { "content-type": "text/plain" },
+    return new Response("V2Ray Worker is running!", {
+      headers: { "content-type": "text/plain" }
     });
-  },
-};
+  }
+}
